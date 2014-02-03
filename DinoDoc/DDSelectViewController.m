5b2a74b2b256ea8a66570e030bb538150e3e0225
Swapnil Takalkar <09apps.id@gmail.com>
2014-02-02 23:11:34 -0700

    Reading from pList

--- DinoDoc/DDSelectViewController.m
+++ DinoDoc/DDSelectViewController.m
@@ -37,6 +37,25 @@
 	// Do any additional setup after loading the view.
     
     NSLog(@"I got this animal %d",self.animaltype);
+    
+    NSString* plistPath = [self getPlistPath:@"TRexQA"];
+    
+    // read property list into memory as an NSData object
+    NSData *plistXML = [[NSFileManager defaultManager] contentsAtPath:plistPath];
+    NSString *errorDesc = nil;
+    NSPropertyListFormat format;
+    // convert static property list into dictionary object
+    NSDictionary *temp = (NSDictionary *)[NSPropertyListSerialization propertyListFromData:plistXML mutabilityOption:NSPropertyListMutableContainersAndLeaves format:&format errorDescription:&errorDesc];
+    
+    if (!temp)
+    {
+        NSLog(@"Error reading plist: %@, format: %d", errorDesc, format);
+    }
+    
+    NSMutableArray* questionarray = [temp objectForKey:@"QArr"];;
+    
+    NSLog(@"Array %@",questionarray);
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -44,5 +63,38 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (NSString*) getPlistPath:(NSString*) pListName
+{
+    // get paths from root direcory
+    NSArray *paths = NSSearchPathForDirectoriesInDomains (NSDocumentDirectory, NSUserDomainMask, YES);
+    // get documents path
+    NSString *documentsPath = [paths objectAtIndex:0];
+    
+    NSString *filename = [pListName stringByAppendingString:@".plist"];
+    
+    // get the path to our Data/plist file
+    NSString *plistPath = [documentsPath stringByAppendingPathComponent:filename];
+    
+    // check to see if Data.plist exists in documents
+    if (![[NSFileManager defaultManager] fileExistsAtPath:plistPath])
+    {
+        NSError *err;
+        // if not in documents, get property list from main bundle
+        NSString* pBundlePath = [[NSBundle mainBundle] pathForResource:pListName ofType:@"plist"];
+        
+        // Copy Plist to document directory
+        NSFileManager* manager = [NSFileManager defaultManager];
+        [manager copyItemAtPath:pBundlePath toPath:plistPath error:&err];
+        
+        if(err)
+        {
+            NSLog(@"Error in saveData: %@", err);
+            return pBundlePath;
+        }
+    }
+    return plistPath;
+}
+
 
 @end
